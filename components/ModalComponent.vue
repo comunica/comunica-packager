4691cec8d0fb9a153b741909409597c5a0a44602
Nathaniel Joos <n_atha_niel@hotmail.com>
2020-10-26 11:13:49 +0100

    Initialize modal

---
+++ components/ModalComponent.vue
@@ -0,0 +1,20 @@
+<template>
+    <div id="modal">
+        <p>Eej maatje</p>
+        <ButtonComponent :is-small="true" @click="$emit('close')"/>
+    </div>
+</template>
+
+<script>
+import ButtonComponent from "@/components/ButtonComponent";
+export default {
+name: "ModalComponent",
+    components: {ButtonComponent}
+}
+</script>
+
+<style scoped>
+    #modal {
+        font-family: "Open Sans", Verdana, Arial, sans-serif;
+    }
+</style>
---
+++ plugins/vue-js-modal.js
@@ -0,0 +1,9 @@
+import Vue from 'vue';
+import VModal from 'vue-js-modal/dist/ssr.nocss';
+
+import 'vue-js-modal/dist/styles.css'
+
+Vue.use(VModal, {
+    dynamicDefaults: {
+    }
+});
