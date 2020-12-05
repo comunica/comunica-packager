d4432c1eca7e083d506420350f548832da88bf95
Nathaniel Joos <n_atha_niel@hotmail.com>
2020-12-05 16:13:03 +0100

    Setup testing for file download

--- cypress/integration/index_page_spec.js
+++ cypress/integration/index_page_spec.js
@@ -1,20 +1,25 @@
 import JSZip from "jszip";
+const path = require('path');
 
 describe('The home (index) page', () => {
-    it('Imports configs', () => {
+    it('imports and successfully downloads config', () => {
+        const downloadDirectory = path.join(__dirname, '..', 'downloads');
+
         cy.visit('/');
-        // Wait 15 seconds to load everything in
-        cy.wait(5000);
+        // Wait 10 seconds to load everything in
+        cy.wait(10000);
         cy.get('#import-conf').click();
         cy.contains('Comunica SPARQL').click();
-        cy.wait(15000);
-        cy.get('#export-conf').click().readFile('/home/nathaniel/Downloads/engine.zip', 'binary').then(
-            (x) => {
-                let zip = new JSZip();
-                zip.loadAsync(x).then(async (z) => {
-                })
-            }
-        );
+        cy.wait(11000);
+        cy.get('#export-conf').click();
+        cy.wait(500);
+        // Assert if file exists
+        cy.readFile(downloadDirectory + '/engine.zip', 'binary');
+            // .then(x =>{
+            //     let zip = new JSZip();
+            //     zip.loadAsync(x).then(async (z) => {
+            //     })
+            // })
 
     });
 });
