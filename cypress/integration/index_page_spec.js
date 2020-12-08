49ffc7666ab32285b480d8b01247962fd04dd97a
Nathaniel Joos <n_atha_niel@hotmail.com>
2020-12-08 13:10:39 +0100

    Delete files after testing

--- cypress/integration/index_page_spec.js
+++ cypress/integration/index_page_spec.js
@@ -3,26 +3,31 @@ const path = require('path');
 
 describe('The home (index) page', () => {
     it('imports and successfully downloads config', () => {
-        const downloadDirectory = path.join(__dirname, '..', 'downloads');
+        const downloadFolder = path.join(__dirname, '..', 'downloads');
+        const zipFile = downloadFolder + '/engine.zip';
+        const engineFolder = downloadFolder + '/engine/';
 
         cy.visit('/');
         // Wait 30 seconds to load everything in
         cy.get('#mediators').should('not.exist');
-        cy.wait(30000);
+        cy.wait(10000);
         // If #mediators exists then mediators are loaded.
         cy.get('#mediators').should('exist');
         cy.get('#import-conf').click();
         cy.contains('Comunica SPARQL').click();
-        cy.wait(30000);
+        cy.wait(500);
         cy.get('#export-conf').click();
         cy.wait(500);
         // Assert if file exists
-        cy.readFile(downloadDirectory + '/engine.zip', 'binary');
-            // .then(x =>{
-            //     let zip = new JSZip();
-            //     zip.loadAsync(x).then(async (z) => {
-            //     })
-            // })
+        cy.readFile(zipFile, 'binary');
+        // Unzip file
+        cy.exec('unzip -d ' + engineFolder + ' ' + zipFile);
+
+
+
+        // Finally delete downloaded zip and unzipped folder
+        cy.exec('rm ' + zipFile);
+        cy.exec('rm -r ' + engineFolder);
 
     });
 });
