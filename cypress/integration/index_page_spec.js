07ee99554c3b422ce10e76c36ff7df63dce97434
Nathaniel Joos <n_atha_niel@hotmail.com>
2020-12-22 15:01:59 +0100

    Resolve path errors

--- cypress/integration/index_page_spec.js
+++ cypress/integration/index_page_spec.js
@@ -1,5 +1,4 @@
 const path = require('path');
-const downloadFolder = path.join(__dirname, '..', 'downloads');
 
 describe('The home (index) page', () => {
     before(() => {
@@ -8,7 +7,7 @@ describe('The home (index) page', () => {
         // Mediators should load
         cy.get('#mediators').should('not.exist');
         // Wait enough seconds to load everything in
-        cy.wait(60000);
+        cy.wait(11000);
         // Mediators should be loaded
         cy.get('#mediators').should('exist');
     });
@@ -24,18 +23,20 @@ describe('The home (index) page', () => {
         const zipFile = downloadFolder + '/engine.zip';
         const engineFolder = downloadFolder + '/engine/';
 
+        cy.exec('echo ' + downloadFolder);
+
         // Open dropdown of import configs
         cy.get('#import-conf').click();
         // Click Comunica SPARQL
         cy.contains('Comunica SPARQL').click();
         // Wait half a second
         cy.wait(500);
-        // Click export
+        // Export
         cy.get('#export-conf').click();
+        cy.get('#btn-export').click();
         // Wait 2 seconds
         cy.wait(2000);
         // Unzip the file for further testing
-        cy.exec('ls ' + downloadFolder);
         cy.exec('unzip -d ' + engineFolder + ' ' + zipFile);
 
     });
